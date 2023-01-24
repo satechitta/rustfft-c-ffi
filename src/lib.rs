ae7f5d994c6140b27c47235bc11fda7d694b2ede
chanutan <chanutan28@gmail.com>
2023-01-24 11:38:10 +0900

    doc: edit some comment to explain each function

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,15 @@ use std::ptr;
 use std::slice;
 use std::sync::Arc;
 
+/// Each instance has std::sync::Arc after initialization.
 pub struct RustFftC {
     fft: Arc<dyn Fft<f32>>,
 }
 impl RustFftC {
+    /// Constructs a new `RustFftC` instance.
+    /// 
+    /// A new instance is initialized by given `fft_size` which is applicable both the power-of-two case and the other cases.  
+    /// If `is_ifft` boolean flag is true, the instance is created to work an inverse FFT.
     fn new(fft_size: usize, is_ifft: bool) -> Self {
         let mut planner: FftPlanner<f32> = FftPlanner::new();
         let fft: Arc<dyn Fft<f32>> = if is_ifft == false {
@@ -17,16 +22,25 @@ impl RustFftC {
         RustFftC { fft: fft }
     }
 
+    /// Returns computed FFT results.
     fn run(&mut self, buffer: &mut Vec<Complex<f32>>) {
         self.fft.process(buffer);
     }
 }
 
+/// FFI interface to initialize `RustFftC` is called by C/C++.
+/// 
+/// Returns a pointer of a `RustFftC` object which is initialized by given `fft_size`
+/// If `is_ifft` boolean flag is true, the `RustFftC` is created to work an inverse FFT.
 #[no_mangle]
-pub extern "C" fn rustfft_new(len: usize, is_ifft: bool) -> *mut RustFftC {
-    Box::into_raw(Box::new(RustFftC::new(len, is_ifft)))
+pub extern "C" fn rustfft_new(fft_size: usize, is_ifft: bool) -> *mut RustFftC {
+    // need catch if fft_size < 0.
+    Box::into_raw(Box::new(RustFftC::new(fft_size, is_ifft)))
 }
 
+/// FFI interface to delete `RustFftC` is called by C/C++.
+/// 
+/// a pointer of RustFftC object argument is deleted by calling the API.
 #[no_mangle]
 pub extern "C" fn rustfft_delete(ptr: *mut RustFftC) {
     if ptr.is_null() {
@@ -37,13 +51,20 @@ pub extern "C" fn rustfft_delete(ptr: *mut RustFftC) {
     }
 }
 
+/// FFI interface to compute FFT using a pointer of `RustFftC` set is called by C/C++.
+/// 
+/// `re_list` is a real number of complex, `im_list` is a imaginary number of complex.
+/// C/C++ side is not requred a dedicated type represented complex number.
+/// The both lists need to have `fft_size` elements.
+/// The value computed FFT replaces `re_list` and `im_list` arguments. 
 #[no_mangle]
 pub extern "C" fn rustfft_run(
     ptr: *mut RustFftC,
     re_list: *mut f32,
     im_list: *mut f32,
-    len: usize,
+    fft_size: usize,
 ) {
+    // need exception handling instead of assert.
     let rustfft_c_ffi: &mut RustFftC = unsafe {
         assert!(!ptr.is_null());
         &mut *ptr
@@ -51,27 +72,27 @@ pub extern "C" fn rustfft_run(
     let mut _re_list: Vec<f32> = unsafe {
         assert!(!re_list.is_null());
 
-        slice::from_raw_parts(re_list, len as usize).to_vec()
+        slice::from_raw_parts(re_list, fft_size as usize).to_vec()
     };
     let mut _im_list: Vec<f32> = unsafe {
         assert!(!im_list.is_null());
 
-        slice::from_raw_parts(im_list, len as usize).to_vec()
+        slice::from_raw_parts(im_list, fft_size as usize).to_vec()
     };
     let mut buffer: Vec<Complex<f32>> = vec![
         Complex {
             re: 0.0f32,
             im: 0.0f32
         };
-        len
+        fft_size
     ];
     for (i, val) in buffer.iter_mut().enumerate() {
         val.re = _re_list[i];
         val.im = _im_list[i];
     }
     rustfft_c_ffi.run(&mut buffer);
-    let mut ret_re_list: Vec<f32> = vec![0.0f32; len];
-    let mut ret_im_list: Vec<f32> = vec![0.0f32; len];
+    let mut ret_re_list: Vec<f32> = vec![0.0f32; fft_size];
+    let mut ret_im_list: Vec<f32> = vec![0.0f32; fft_size];
     for (i, val) in buffer.iter().enumerate() {
         ret_re_list[i] = val.re;
         ret_im_list[i] = val.im;
@@ -79,8 +100,8 @@ pub extern "C" fn rustfft_run(
     let ret_re_list_ptr: *const f32 = ret_re_list[..].as_ptr();
     let ret_im_list_ptr: *const f32 = ret_im_list[..].as_ptr();
     unsafe {
-        ptr::copy(ret_re_list_ptr, re_list, len);
-        ptr::copy(ret_im_list_ptr, im_list, len);
+        ptr::copy(ret_re_list_ptr, re_list, fft_size);
+        ptr::copy(ret_im_list_ptr, im_list, fft_size);
     }
 }
 
@@ -126,7 +147,7 @@ fn test_rustfft_c_ffi() {
         original_fft_buffer_im[i] = val.im;
     }
 
-    // comparison between c ffi rustfft and original one
+    // compare between c ffi rustfft results and original one
     for (i, ans) in original_fft_buffer_re.iter().enumerate() {
         assert_eq!(&buffer_re[i], ans);
     }
